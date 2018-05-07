c3675a7616010e2dcfa868cbe0c60be32e40f46f
Swarna Rout <swarna.p.rout@globallogic.com>
2018-05-07 12:34:56 +0530

    'mocha'

---
+++ mocha.test.js
@@ -0,0 +1,5 @@
+var test=require('./testing');
+var mocha=require('mocha');
+it('should add two numbers',()=>{
+    var res=test.add(11,12);
+})
---
+++ testing.js
@@ -0,0 +1,6 @@
+var add=(a,b)=>{
+return a+b;
+}
+module.exports={
+    add
+}
